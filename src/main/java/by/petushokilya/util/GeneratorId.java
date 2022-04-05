804a6d1f961c072320d8cae6c8496ba23997c031
Petushok Ilya <a3310010752@mail.ru>
2022-04-06 00:56:19 +0300

    update generator

--- src/main/java/by/petushokilya/util/GeneratorId.java
+++ src/main/java/by/petushokilya/util/GeneratorId.java
@@ -1,9 +1,14 @@
 package by.petushokilya.util;
 
 public class GeneratorId {
-    public static int id=0;
+    public static int idTruck=0;
+    public static int idTerminal=0;
 
-    public static int generatorId(){
-        return ++id;
+    public static int generatorIdTruck(){
+        return ++idTruck;
+    }
+
+    public static int generatorIdTerminal(){
+        return ++idTerminal;
     }
 }
