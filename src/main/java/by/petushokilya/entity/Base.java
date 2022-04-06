46932184eab8639141b566f31f1df32d1c1539f7
Petushok Ilya <a3310010752@mail.ru>
2022-04-06 18:28:54 +0300

    message

--- src/main/java/by/petushokilya/entity/Base.java
+++ src/main/java/by/petushokilya/entity/Base.java
@@ -1,5 +1,8 @@
 package by.petushokilya.entity;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.concurrent.CountDownLatch;
@@ -9,66 +12,70 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Base {
+    private static final Logger logger = LogManager.getLogger(Base.class.getName());
+
     public static Base instance;
-    private static final ReentrantLock lock=new ReentrantLock(true);
-    private static final CountDownLatch initializingLatch =new CountDownLatch(1);
-    private static final AtomicBoolean isInstanceInitialized=new AtomicBoolean(false);
-    private static final int FREE_PLACE_FOR_BASE=10;
+
+    private static final ReentrantLock lock = new ReentrantLock(true);
+    private static final CountDownLatch initializingLatch = new CountDownLatch(1);
+    private static final AtomicBoolean isInstanceInitialized = new AtomicBoolean(false);
+    private static final Condition condition = lock.newCondition();
+    private static final int FREE_PLACE_FOR_BASE = 3;
     private final Deque<Terminal> freeTerminals;
-    private Condition condition= lock.newCondition();
     private AtomicInteger quantityProductOnBase;
 
     public AtomicInteger getQuantityProductOnBase() {
         return quantityProductOnBase;
     }
 
-    public static Base getInstance(){
-        if(instance==null){
-            while (isInstanceInitialized.compareAndSet(false,true)){
-             instance=new Base();
-             initializingLatch.countDown();
+    public static Base getInstance() {
+        if (instance == null) {
+            while (isInstanceInitialized.compareAndSet(false, true)) {
+                instance = new Base();
+                initializingLatch.countDown();
             }
             try {
                 initializingLatch.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            } catch (InterruptedException exception) {
+                logger.error(exception);
             }
         }
         return instance;
     }
 
-    private Base(){
-        freeTerminals=new ArrayDeque<>();
-        quantityProductOnBase=new AtomicInteger();
-        for(int i=0;i<FREE_PLACE_FOR_BASE;i++){
-            Terminal terminal=new Terminal();
+    private Base() {
+        freeTerminals = new ArrayDeque<>();
+        quantityProductOnBase = new AtomicInteger();
+        for (int i = 0; i < FREE_PLACE_FOR_BASE; i++) {
+            Terminal terminal = new Terminal();
             freeTerminals.add(terminal);
         }
     }
 
-    public Terminal getFreeTerminal(Truck truck){
+    public Terminal getFreeTerminal(Truck truck) {
         Terminal terminal = null;
         truck.setState(Truck.State.Waiting);
         try {
             lock.lock();
-            while(freeTerminals.size()==0){
-               condition.await();
+            while (freeTerminals.size() == 0) {
+                logger.info("Truck "+truck.getIdTruck()+",state "+truck.getState());
+                condition.await();
             }
-            terminal=freeTerminals.poll();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            terminal = freeTerminals.poll();
+        } catch (InterruptedException exception) {
+            logger.error(exception);
         } finally {
             lock.unlock();
         }
         return terminal;
     }
 
-    public void realizedTerminal(Terminal terminal){
+    public void realizedTerminal(Terminal terminal) {
         lock.lock();
         try {
             freeTerminals.add(terminal);
             condition.signalAll();
-        }finally {
+        } finally {
             lock.unlock();
         }
     }
--- src/main/java/by/petushokilya/entity/Truck.java
+++ src/main/java/by/petushokilya/entity/Truck.java
@@ -1,17 +1,21 @@
 package by.petushokilya.entity;
 
+import by.petushokilya.exception.CustomException;
+import by.petushokilya.main.Main;
 import by.petushokilya.util.GeneratorId;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Truck implements Runnable {
+    private static final Logger logger= LogManager.getLogger(Truck.class.getName());
     private Random random = new Random();
     private int idTruck;
     private int capacity;
     private int occupationCapacity;
     private boolean isLoad;
-    // private boolean priority;
     private State state;
 
     public enum State {
@@ -20,9 +24,9 @@ public class Truck implements Runnable {
 
     {
         idTruck = GeneratorId.generatorIdTruck();
-        //   priority=random.nextBoolean();
         isLoad = random.nextBoolean();
         state = State.New;
+        logger.info("Truck number "+this.idTruck+", state "+this.state);
     }
 
     public Truck() {
@@ -49,14 +53,6 @@ public class Truck implements Runnable {
         this.capacity = capacity;
     }
 
-//    public boolean isPriority() {
-//        return priority;
-//    }
-//
-//    public void setPriority(boolean priority) {
-//        this.priority = priority;
-//    }
-
     public State getState() {
         return state;
     }
@@ -81,7 +77,7 @@ public class Truck implements Runnable {
             terminal = base.getFreeTerminal(this);
             processLoadOrUnload(terminal, base);
         } catch (InterruptedException exception) {
-
+            logger.error("Mistake method run"+exception);
         } finally {
             base.realizedTerminal(terminal);
         }
@@ -89,6 +85,7 @@ public class Truck implements Runnable {
 
     private void processLoadOrUnload(Terminal terminal, Base base) throws InterruptedException {
         this.state = State.Runnable;
+        logger.info("Truck number"+this.idTruck+",state "+this.state);
         if (this.isLoad) {
             load(base);
         } else {
@@ -101,13 +98,17 @@ public class Truck implements Runnable {
         int quantityForLoad;
         quantityForLoad = this.capacity - this.occupationCapacity;
         base.getQuantityProductOnBase().getAndAdd(-quantityForLoad);
+        logger.info("Truck number "+this.idTruck+" come to load.");
         TimeUnit.SECONDS.sleep(3);
         this.state = State.Terminate;
+        logger.info("Truck "+this.idTruck+",state "+this.state);
     }
 
     private void unload(Base base) throws InterruptedException {
         base.getQuantityProductOnBase().getAndAdd(this.occupationCapacity);
+        logger.info("Truck number "+this.idTruck+" come to unload.");
         TimeUnit.SECONDS.sleep(3);
         this.state = State.Terminate;
+        logger.info("Truck "+this.idTruck+",state "+this.state);
     }
 }
